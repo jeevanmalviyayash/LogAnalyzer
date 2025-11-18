48e7fd9d9b3975a3954c7cb16a1fe7e755dbec6b
nihalsherkarYash <nihalahmad.sherkar@yash.com>
2025-11-18 11:09:35 +0530

    add swagger documentation

--- src/main/java/com/yash/log/LogApplication.java
+++ src/main/java/com/yash/log/LogApplication.java
@@ -1,7 +1,22 @@
 package com.yash.log;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Log Analysis REST API Documentation",
+				description = "This is Log Analysis REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+								name = "Nihalahmad Aslam Sherkar",
+								email = "nihalahmad.shekar@yash.com"
+						)
+		)
+)
 
 @SpringBootApplication
 public class LogApplication {
--- src/main/java/com/yash/log/controller/ErrorLogController.java
+++ src/main/java/com/yash/log/controller/ErrorLogController.java
@@ -1,23 +1,29 @@
 package com.yash.log.controller;
 
-import com.yash.log.entity.ErrorLog;
+
 import com.yash.log.repository.ErrorLogRepository;
 import com.yash.log.service.services.LogFileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDateTime;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+@Tag(
+        name = "Error Log Management APIs",
+        description = "APIs for managing and analyzing error logs"
+)
 @RestController
 @RequestMapping("/api/errors")
 public class ErrorLogController {
@@ -28,6 +34,11 @@ public class ErrorLogController {
     @Autowired
     private LogFileService logFileService;
 
+    @Operation(
+            summary = "Get Error Statistics",
+            description = "Retrieve statistics of error logs grouped by error level"
+
+    )
     @GetMapping("/stats")
     public Map<String,Long> getErrorStats(){
         List<Object[]> results = errorLogRepository.countErrorsByLevel();
@@ -40,7 +51,20 @@ public class ErrorLogController {
 
 
 
-
+    @Operation(
+            summary = "Upload Log File",
+            description = "Upload a log file to parse and store error logs"
+    )
+    @ApiResponses({
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "HTTP status OK"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal Server Error"
+                    )
+            })
     @PostMapping("/upload")
     public ResponseEntity<String> uploadLogFile(@RequestParam("file") MultipartFile file) {
         try {
@@ -63,7 +87,14 @@ public class ErrorLogController {
         }
     }
 
-
+    @Operation(
+            summary = "Get Chart Data",
+            description = "Retrieve error log data formatted for chart visualization"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status OK"
+    )
     @GetMapping("/chart-data")
     public List<Map<String, Object>> getChartData() {
         List<Object[]> results = errorLogRepository.countErrorsByLevel();
