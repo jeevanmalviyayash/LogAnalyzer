4aa29f54c9e19ad65aa443a210498634c797a74b
Jeevan Malviya <jeevan.malviya@yash.com>
2025-11-26 15:49:53 +0530

    change in ErrorLogController for cross origin issue resolve and some commeted code

--- src/main/java/com/yash/log/controller/ErrorLogController.java
+++ src/main/java/com/yash/log/controller/ErrorLogController.java
@@ -24,7 +24,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/errors")
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ErrorLogController {
 
     private final LogFileServiceImpl logFileServiceImpl;
